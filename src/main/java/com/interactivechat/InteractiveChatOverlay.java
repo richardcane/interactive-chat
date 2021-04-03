72752c74bf22afb4f71fe0140b9bf6da94fc3493
Richard Cane <29371470+richardcane@users.noreply.github.com>
2021-04-03 02:13:09 +0100

    Initial concept

---
+++ src/main/java/com/interactivechat/InteractiveChatOverlay.java
@@ -0,0 +1,158 @@
+package com.interactivechat;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.util.Optional;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+import javax.inject.Inject;
+
+import net.runelite.api.Client;
+import net.runelite.api.FontTypeFace;
+import net.runelite.api.ScriptEvent;
+import net.runelite.api.widgets.JavaScriptCallback;
+import net.runelite.api.widgets.Widget;
+import net.runelite.api.widgets.WidgetInfo;
+import net.runelite.api.widgets.WidgetType;
+import net.runelite.client.ui.overlay.Overlay;
+import net.runelite.client.ui.overlay.OverlayPosition;
+import net.runelite.client.util.LinkBrowser;
+import net.runelite.client.util.Text;
+import okhttp3.HttpUrl;
+
+class InteractiveChatOverlay extends Overlay {
+	private final Client client;
+	private final InteractiveChatConfig config;
+
+	static final HttpUrl WIKI_BASE = HttpUrl.parse("https://oldschool.runescape.wiki");
+	static final Pattern SEARCH_PATTERN = Pattern.compile("((?<=\\])|(?=\\[))", Pattern.DOTALL);
+	static final String LEFT_DELIMITER = "[";
+	static final String RIGHT_DELIMITER = "]";
+
+	static Widget chatbox;
+	static Widget hitbox;
+	static String search = "";
+
+	@Inject
+	InteractiveChatOverlay(Client client, InteractiveChatConfig config) {
+		this.client = client;
+		this.config = config;
+
+		setPosition(OverlayPosition.DYNAMIC);
+	}
+
+	private void createHitbox() {
+		chatbox = getChatbox();
+		hitbox = chatbox.createChild(-1, WidgetType.RECTANGLE);
+
+		hitbox.setOpacity(255);
+		hitbox.setOriginalX(0);
+		hitbox.setOriginalY(0);
+		hitbox.setOriginalWidth(0);
+		hitbox.setOriginalHeight(14);
+		hitbox.setNoClickThrough(true);
+
+		hitbox.setAction(0, "Open");
+		hitbox.setName("Wiki");
+
+		hitbox.setHasListener(true);
+		hitbox.setOnClickListener((JavaScriptCallback) this::search);
+
+		hitbox.revalidate();
+	}
+
+	private void positionHitbox(int x, int y, int width) {
+		hitbox.setOriginalX(x);
+		hitbox.setOriginalY(y);
+		hitbox.setOriginalWidth(width);
+
+		hitbox.revalidate();
+	}
+
+	@Override
+	public Dimension render(Graphics2D graphics) {
+		if (hitbox == null) {
+			createHitbox();
+		}
+		positionHitbox(0, 0, 0);
+
+		Widget chatLine = this.getHoveredChatline();
+		if (client.isMenuOpen() || chatLine == null || chatLine.getWidth() == 486) {
+			return null;
+		}
+
+		final FontTypeFace font = chatLine.getFont();
+		final String text = Text.removeFormattingTags(chatLine.getText());
+		final int textWidth = font.getTextWidth(text);
+		final Rectangle outerBounds = chatLine.getBounds();
+
+		final net.runelite.api.Point mouse = this.client.getMouseCanvasPosition();
+		final Point mousePoint = new Point(mouse.getX(), mouse.getY());
+
+		if (textWidth <= outerBounds.width) {
+			String[] parts = SEARCH_PATTERN.split(text);
+
+			int xForBounds = (int) outerBounds.getMinX();
+			int xForHitbox = chatLine.getOriginalX();
+			for (String part : parts) {
+				final int partWidth = font.getTextWidth(part);
+
+				if (!part.startsWith(LEFT_DELIMITER)) {
+					xForBounds += partWidth;
+					xForHitbox += partWidth;
+					continue;
+				}
+
+				Rectangle partBounds = new Rectangle(xForBounds, (int) outerBounds.getMinY() + 1, partWidth,
+						outerBounds.height);
+
+				if (partBounds.contains(mousePoint)) {
+					search = part.replace(LEFT_DELIMITER, "").replace(RIGHT_DELIMITER, "");
+
+					positionHitbox(xForHitbox, chatLine.getOriginalY() + 1, partWidth);
+
+					graphics.setPaint(Color.GREEN);
+					graphics.fill(new Rectangle(partBounds.x + 2, partBounds.y + partBounds.height - 1, partBounds.width - 4, 1));
+				}
+
+				xForBounds += partWidth;
+				xForHitbox += partWidth;
+			}
+		}
+
+		return null;
+	}
+
+	private Widget getHoveredChatline() {
+		chatbox = getChatbox();
+
+		Optional<Widget> optional = Stream.of(chatbox.getChildren()).filter(widget -> !widget.isHidden())
+				.filter(widget -> widget.getName() != "Wiki")
+				.filter(widget -> widget.getId() < WidgetInfo.CHATBOX_FIRST_MESSAGE.getId()).filter(widget -> {
+					int mouseY = this.client.getMouseCanvasPosition().getY();
+					return (mouseY >= widget.getBounds().getMinY() && mouseY <= widget.getBounds().getMaxY());
+				}).skip(1).findFirst();
+
+		if (!optional.isPresent()) {
+			return null;
+		}
+		return optional.get();
+	}
+
+	private Widget getChatbox() {
+		if (chatbox != null) {
+			return chatbox;
+		}
+
+		return this.client.getWidget(WidgetInfo.CHATBOX_MESSAGE_LINES);
+	}
+
+	protected void search(ScriptEvent ev) {
+		LinkBrowser.browse(WIKI_BASE.newBuilder().addQueryParameter("search", search).build().toString());
+	}
+
+}
--- src/main/java/com/interactivechat/InteractiveChatPlugin.java
+++ src/main/java/com/interactivechat/InteractiveChatPlugin.java
@@ -1,16 +1,24 @@
 package com.interactivechat;
 
 import com.google.inject.Provides;
+
+import java.awt.Color;
+import java.util.regex.Pattern;
+
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
-import net.runelite.api.GameState;
-import net.runelite.api.events.GameStateChanged;
+import net.runelite.api.MessageNode;
+import net.runelite.api.events.ChatMessage;
+import net.runelite.client.chat.ChatColorType;
+import net.runelite.client.chat.ChatMessageBuilder;
+import net.runelite.client.chat.ChatMessageManager;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.ui.overlay.OverlayManager;
 
 @Slf4j
 @PluginDescriptor(
@@ -18,36 +26,74 @@ import net.runelite.client.plugins.PluginDescriptor;
 )
 public class InteractiveChatPlugin extends Plugin
 {
+	private static final Pattern SEARCH_PATTERN = Pattern.compile("((?<=\\])|(?=\\[))", Pattern.DOTALL);
+	private static final String LEFT_DELIMITER = "[";
+	private static final String RIGHT_DELIMITER = "]";
+	private final Color LINK_COLOR = Color.GREEN;
+
+	@Inject
+	private ChatMessageManager chatMessageManager;
+
 	@Inject
 	private Client client;
 
 	@Inject
 	private InteractiveChatConfig config;
 
-	@Override
-	protected void startUp() throws Exception
-	{
-		log.info("Example started!");
-	}
+	@Inject
+	private InteractiveChatOverlay overlay;
 
-	@Override
-	protected void shutDown() throws Exception
-	{
-		log.info("Example stopped!");
-	}
-
-	@Subscribe
-	public void onGameStateChanged(GameStateChanged gameStateChanged)
-	{
-		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
-		{
-			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says " + config.greeting(), null);
-		}
-	}
+	@Inject
+	private OverlayManager overlayManager;
 
 	@Provides
 	InteractiveChatConfig provideConfig(ConfigManager configManager)
 	{
 		return configManager.getConfig(InteractiveChatConfig.class);
+	}
+
+	@Override
+	protected void startUp() throws Exception
+	{
+		overlayManager.add(overlay);
+	}
+
+	@Override
+	protected void shutDown() throws Exception
+	{
+		overlayManager.remove(overlay);
+	}
+
+	@Subscribe
+	public void onChatMessage(ChatMessage chatMessage)
+	{
+		final ChatMessageType type = chatMessage.getType();
+		if (type != ChatMessageType.FRIENDSCHAT &&
+				type != ChatMessageType.PRIVATECHAT &&
+				type != ChatMessageType.PUBLICCHAT)
+		{
+			return;
+		}
+
+		String messageContent = chatMessage.getMessage();
+		String[] parts = SEARCH_PATTERN.split(messageContent);
+
+		ChatMessageBuilder builder = new ChatMessageBuilder();
+		for (String part : parts)
+		{
+			if (!part.startsWith(LEFT_DELIMITER))
+			{
+				builder.append(ChatColorType.NORMAL);
+				builder.append(part);
+				continue;
+			}
+
+			builder.append(LINK_COLOR, part.trim());
+		}
+
+		final MessageNode messageNode = chatMessage.getMessageNode();
+		messageNode.setRuneLiteFormatMessage(builder.build());
+		chatMessageManager.update(messageNode);
+		client.refreshChat();
 	}
 }
