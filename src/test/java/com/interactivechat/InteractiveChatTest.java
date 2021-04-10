9fdc4b15fd46837f3bf3e710bf9279d96fa07ec9
Richard Cane <29371470+richardcane@users.noreply.github.com>
2021-04-10 14:20:38 +0100

    Suppress warnings from test runner for now

--- src/test/java/com/interactivechat/InteractiveChatTest.java
+++ src/test/java/com/interactivechat/InteractiveChatTest.java
@@ -1,15 +1,12 @@
 package com.interactivechat;
 
-import com.interactivechat.InteractiveChatPlugin;
-
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class InteractiveChatTest
-{
-	public static void main(String[] args) throws Exception
-	{
-		ExternalPluginManager.loadBuiltin(InteractiveChatPlugin.class);
-		RuneLite.main(args);
-	}
+public class InteractiveChatTest {
+  @SuppressWarnings("unchecked")
+  public static void main(String[] args) throws Exception {
+    ExternalPluginManager.loadBuiltin(InteractiveChatPlugin.class);
+    RuneLite.main(args);
+  }
 }
